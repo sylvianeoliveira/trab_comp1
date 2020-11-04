f28828f9dfcd3b93aa325c99aea8bae1d4b3f84d
sylviane <sylvianesno@dcc.ufrj.br>
2020-11-04 17:32:04 -0300

    Atualizacoes no programa

--- funcoes.h
+++ funcoes.h
@@ -3,20 +3,20 @@
 #include <locale.h>
 #include "estruturas.h"
 
-void acesso_caractere_especial(void);
-void limpa_buffer(void);
+void acesso_caractere_especial(void); /*Permite imprimir caracteres especiais na tela*/
+void limpa_buffer(void); /*Limpa o buffer em diferentes sistemas operacionais*/
 
 void limpa_string(char string[]); /*Retira o \n da string*/
-void limpa_ponteiros(VETORES vetor);
+void limpa_ponteiros(VETORES vetor); /*Zera os ponteiros*/
 
-int valida_nome(char nickname[]); /*!!!!!! falar com o grupo se compensa fazer isso daqui so que usando funcao recursiva(diminuiria o codigo principal)  !!!!!!*/
+int valida_nome(char nickname[]); /*Valida o nome inserido pelo usuário*/
 int valida_acesso_regras(void); /*Valida o acesso a regras*/
 int valida_nivel(void); /*Valida o nivel de jogo*/
-int valida_tentativa (VETORES vetor, int intervalo, int tamanho);
+int valida_tentativa (VETORES vetor, int intervalo); /*Valida cada tentativa feita pelo usuário no jogo*/
 
-void easteregg(char nickname[]);
+void easteregg(char nickname[]); /*Trata e verifica o caso de easteregg*/
 void gerador_senha(VETORES vetor,int nivel); /*Gera uma senha de acordo com o nível escolhido*/
-void calcula_acertos(VETORES vetor, int tamanho); /*Retorna o vetor com a legenda de acertos*/
-int compara_tentativa_senha(VETORES vetor, int tamanho); /*Verifica se a tentativa é igual a senha*/
+void calcula_acertos(VETORES vetor); /*Retorna o vetor com a legenda de acertos*/
+int compara_tentativa_senha(VETORES vetor); /*Verifica se a tentativa é igual a senha*/
 
 #endif
